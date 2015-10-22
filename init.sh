9179a40fd51d2f460bd9404b6dbfbc4c7c071774
Luke <luke@dizzi.net>
2015-10-22 23:00:21 +1100

    startup scripts run cdht python

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
-xterm -hold -title "Peer 1" -e "java cdht 1 3 4" &
-xterm -hold -title "Peer 3" -e "java cdht 3 4 5" &
-xterm -hold -title "Peer 4" -e "java cdht 4 5 8" &
-xterm -hold -title "Peer 5" -e "java cdht 5 8 10" &
-xterm -hold -title "Peer 8" -e "java cdht 8 10 12" &
-xterm -hold -title "Peer 10" -e "java cdht 10 12 15" &
-xterm -hold -title "Peer 12" -e "java cdht 12 15 1" &
-xterm -hold -title "Peer 15" -e "java cdht 15 1 3" &
+xterm -hold -title "Peer 1" -e "`pwd`/cdht 1 3 4" &
+xterm -hold -title "Peer 3" -e "`pwd`/cdht 3 4 5" &
+xterm -hold -title "Peer 4" -e "`pwd`/cdht 4 5 8" &
+xterm -hold -title "Peer 5" -e "`pwd`/cdht 5 8 10" &
+xterm -hold -title "Peer 8" -e "`pwd`/cdht 8 10 12" &
+xterm -hold -title "Peer 10" -e "`pwd`/cdht 10 12 15" &
+xterm -hold -title "Peer 12" -e "`pwd`/cdht 12 15 1" &
+xterm -hold -title "Peer 15" -e "`pwd`/cdht 15 1 3" &
